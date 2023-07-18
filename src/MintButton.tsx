49425980bcd5d3e99be4160e05e03f8c42af3ed6
Zobeir Hamid <zobeir@usesafar.com>
2023-07-18 16:46:03 -0700

    feat: updates example

--- src/MintButton.tsx
+++ src/MintButton.tsx
@@ -1,27 +1,36 @@
-import { useCallback } from "react"
-import { Interface } from "ethers/lib/utils"
+import { useCallback, useState } from "react"
 import abi from './0x34bE7f35132E97915633BC1fc020364EA5134863.json'
 import { useSmartPrivy } from '@zerodevapp/privy'
+import { encodeFunctionData } from "viem"
+
 
 const contractAddress = '0x34bE7f35132E97915633BC1fc020364EA5134863'
-const contractInterface = new Interface(abi)
 
 const MintButton: React.FC = () => {
+    const [loading, setLoading] = useState(false)
+
     const smartPrivy = useSmartPrivy()
 
     const handleMint = useCallback(() => {
-        if (smartPrivy.user?.wallet?.address !== undefined && smartPrivy.sendTransaction) {
+      console.log(smartPrivy.user?.wallet?.address !== undefined && !!smartPrivy.sendTransaction)
+        if (smartPrivy.user?.wallet?.address !== undefined && !!smartPrivy.sendTransaction) {
+            setLoading(true)
             smartPrivy.sendTransaction({
               to: contractAddress,
-              data: contractInterface.encodeFunctionData('mint', [smartPrivy.user.wallet.address]),
+              data: encodeFunctionData({
+                abi,
+                functionName: 'mint',
+                args: [smartPrivy.user.wallet.address],
+              })
             }).then((receipt: any) => {
+              setLoading(false)
               console.log(receipt)
             })
           }
     }, [smartPrivy])
 
     return (
-        <button onClick={handleMint} style={{ marginTop: "20px", padding: "12px", backgroundColor: "#069478", color: "#FFF", border: "none", borderRadius: "6px", cursor: 'pointer' }}>
+        <button disabled={loading} onClick={handleMint}>
             Mint
         </button>
     )
